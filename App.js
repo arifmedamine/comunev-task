c7180ca359cc7aef4981e7f12f139bd5d5c8e39c
arifmedamine <mohamedamine.arif@esprit.tn>
2021-04-11 01:03:49 +0100

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
-export default function App() {
-  return (
+export default class App extends Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      dataSource: []
+    }
+  }
+
+  renderItem = ({item}) => {
+    return(
+      <View style={{flex: 1 , flexDirection: 'row', margin: '20%'}}> 
+      <Text>
+        {item.name.title}
+      </Text>
+      <Text>
+        {item.name.first}
+      </Text>
+      <Text>
+        {item.name.last}
+      </Text>
+    </View>
+    )
+  }
+  
+  componentDidMount() {
+    const url = "https://randomuser.me/api/?results=100&inc=name";
+    fetch(url)
+      .then((response) => response.json())
+      .then((responseJson) => {
+        this.setState({
+          dataSource: responseJson.results
+        })
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+  
+  render() {
+    return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        data={this.state.dataSource}
+        renderItem={this.renderItem}
+      />
     </View>
   );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
